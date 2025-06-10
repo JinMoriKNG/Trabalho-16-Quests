bc5cfd5f311460cae2d3d1c92917459a35f54594
JinMoriKNG <kakarottossj9@gmail.com>
2025-06-09 21:21:02 -0300

    Add files via upload

---
+++ Proz/ex7 troca.py
@@ -0,0 +1,10 @@
+print('=-'*16, 'TROCA DE VALORES', '=-'*16)
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+
+num1 = num2
+num2 = num1
+
+print(num1)
+print(num2)
