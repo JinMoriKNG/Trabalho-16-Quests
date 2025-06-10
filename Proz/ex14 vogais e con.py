47a75d6e0b9ee191e20c8d8b53deef6164701596
JinMoriKNG <kakarottossj9@gmail.com>
2025-06-10 08:36:26 -0300

    Update ex14 vogais e con.py

--- Proz/ex14 vogais e con.py
+++ Proz/ex14 vogais e con.py
@@ -2,7 +2,7 @@ print('-'*10, 'VOGAIS E CONSOANTES', '-'*10)
 
 letra = input('Digite uma letra para verificação: ').strip().upper()
 
-if letra not in 'AEIOU':
+if letra in 'AEIOU':
     print('Essa letra é uma VOGAL.')
 else:
     print('Essa letra é uma CONSOANTE')
