7024fecf224a334fd8b09959e8a643e55704ab9f
JinMoriKNG <kakarottossj9@gmail.com>
2025-06-10 08:37:13 -0300

    Update ex15 desconto100.py

--- Proz/ex15 desconto100.py
+++ Proz/ex15 desconto100.py
@@ -3,7 +3,7 @@ print('-'*10, 'DESCONTO 100R$', '-'*10)
 preço = float(input('Informe o preço para verificação: '))
 
 if preço > 100:
-    desconto = preço / 25 * 100
+    desconto = preço * 25 / 100
     print('Este preço tem um desconto de 25%')
     print(f'Com isso o preço muda para {preço - desconto}R$')
 else:
