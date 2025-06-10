ecf343147552e45111a1630f626dde7daa4c3439
JinMoriKNG <kakarottossj9@gmail.com>
2025-06-10 08:35:32 -0300

    Update ex8 negativos.py

--- Proz/ex8 negativos.py
+++ Proz/ex8 negativos.py
@@ -7,4 +7,4 @@ for i in range(1,5):
     if num < 0:
         cont += 1
 
-print(f'A quantidade de números digitados foi de {cont} números.')
+print(f'A quantidade de números negativos digitados foi de {cont} números.')
